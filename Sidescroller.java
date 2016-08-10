f543b55ad2f069e1798b0f7dc43b315dacdfa275
drk343 <drk343@users.noreply.github.com>
2016-08-10 14:02:45 -0500

    Update and rename Main.java to Sidescroller.java

---
+++ Sidescroller.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class Sidescroller {
+  
+  public void run() {
+    // TODO insert run code here
+  }
+  
+  public static void main(String args[]) {
+    new Thread(new Sidescroller() ).start();
+  }
+}
